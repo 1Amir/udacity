f270d0aac9d1f2db08c012ea0173a33bdf81c2dd
amir computing software <amirandshamar@gmail.com>
2017-07-27 11:38:43 -0700

    Update main.py

--- .gitignore/main.py
+++ .gitignore/main.py
@@ -1,6 +1,9 @@
 import time
 import webbrowser
-life = True
-while life == True:
+breaks = 0
+totalamount = 5
+print "this program started at "+ time.ctime()
+while breaks < totalamount:
   time.sleep(7200)
   webbrowser.open("https://www.youtube.com/watch?v=vE2ETqUGj6Q")
+  breaks = breaks + 1
