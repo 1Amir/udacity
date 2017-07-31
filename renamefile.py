b57208da88429b324bdda8b9b47e918c23272728
1Amir <amirandshamar@gmail.com>
2017-07-30 18:10:49 -0700

    Inital commit

--- renamefile.py
+++ renamefile.py
@@ -1,5 +1,12 @@
 import os, sys
-# Open a file
-path = "/home"
-file_list = os.listdir( path )
+#Open a file
+def renamefile():
+    file_list = os.listdir("/home/amir/udacity/renamefileproject")
+    print file_list
 
+    #Rename all the files.
+    os.chdir("/home/amir/udacity/renamefileproject")
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+	print file_name
+renamefile()  
